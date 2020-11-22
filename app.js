a163185e43fa696d4af0bbf658c30ae72679eed2
Jose Ramón Vieites Romay <fabelaretorcida@gmail.com>
2020-11-22 07:19:00 +0100

    Version propia

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
 timeline.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
-timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 0.5 });
+timeline.to('.slider', { y: '-100%', duration: 1.5, delay: 1 });
 timeline.to('.intro', { y: '-100%', duration: 1 }, '-=1');
 timeline.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
 timeline.fromTo(
